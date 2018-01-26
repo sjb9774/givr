3cd5ccf2ba713067d8a7c0cda6a41ddec3e23db8
Stephen Biston <sjb9774@gmail.com>
2018-01-26 17:06:13 -0600

    stub out open endpoint

--- givr/endpoints.py
+++ givr/endpoints.py
@@ -3,11 +3,20 @@ from givr.room import WebSocketRoom
 from givr.user import User
 from flask import request, jsonify
 
-@app.route("/api/room/open", methods=["POST"])
-def api_room_open():
+@app.route("/api/room/create", methods=["POST"])
+def api_room_create():
     data = request.get_json()
     owner = User.from_user_id(data.get("owner"))
     room = WebSocketRoom()
     room.open()
     room.add_owner(owner)
     return jsonify({"room_id": room.room_id, "owner_id": owner.user_id})
+
+@app.route("/api/room/open", methods=["POST"])
+def api_room_open():
+    data = request.get_json()
+    user_id = data.get("user_id")
+    room_id = data.get("room_id")
+    room_instance = WebSocketRoom.get_from_id(room_id)
+    room_instance.open()
+    return jsonify({"success": True})
